f56240bc572ae5ed395c9eed311752c4edb6d7f1
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2024-03-17 00:52:19 -0700

    GH-1

--- config/playground-admin.php
+++ config/playground-admin.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 return [
+    'about' => (bool) env('PLAYGROUND_ADMIN_ABOUT', true),
     'load' => [
         'migrations' => (bool) env('PLAYGROUND_ADMIN_LOAD_MIGRATIONS', false),
     ],
